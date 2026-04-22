33ba76abbf768f42c03dfad53c239bb7d7954ec0
kalanak <klana.20234122@iit.ac.lk>
2026-04-22 15:18:37 +0530

    new versal deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://iron-paradise-h4hrcfcxecaycccs.southeastasia-01.azurewebsites.net', // Your actual Azure Backend URL
+    apiUrl: 'https://gym-mangement-system-cp-iron-paradise.onrender.com', // Your actual Azure Backend URL
     sessionTimeout: 6 * 60 * 60 * 1000, // 30 minutes in milliseconds (default)
     useCookies: true, // Use cookies for session storage instead of localStorage
     cookieName: 'sessionToken', // Cookie name for session token
